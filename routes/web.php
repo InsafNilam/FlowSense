cf4d95d09af70f630fa97082b42388a553114cf6
InsafNilam <insafnilam.2000@gmail.com>
2024-06-07 23:20:47 +0530

    feat: Add routes for customer, water bill, and water bill unit resources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WaterBillController;
+use App\Http\Controllers\WaterBillUnitController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Added the following routes
+    Route::resource('customer', CustomerController::class);
+    Route::resource('water-bill', WaterBillController::class);
+    Route::resource('water-bill-unit', WaterBillUnitController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
