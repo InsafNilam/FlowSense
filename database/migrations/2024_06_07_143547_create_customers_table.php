dca1a8c8d2f185f786e89fef7bd027b52ea1c1b8
InsafNilam <insafnilam.2000@gmail.com>
2024-06-08 10:49:15 +0530

    refactor: Update foreign key constraint in water bills migration

--- database/migrations/2024_06_07_143547_create_customers_table.php
+++ database/migrations/2024_06_07_143547_create_customers_table.php
@@ -12,7 +12,6 @@ return new class extends Migration {
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('address');
             $table->string('username')->unique();
             $table->string('bill_no')->unique();
